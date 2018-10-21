5c9b30edabc0c3aba2b277df2ab854150d78d78e
Kowalewski Maksymilian <kowalewski.maksymilian@gmail.com>
2018-10-21 15:56:57 +0200

    moving view to right directory

--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -3,18 +3,16 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ExampleTest extends TestCase
+class ThreadTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_browse_threads()
     {
-        $response = $this->get('/');
+        $response = $this->get('/threads');
 
         $response->assertStatus(200);
     }
